03a3fed06f44a847e1de8d9e61a89e5b67e493d3
navonsanjuni <navonsanjuni178@gmail.com>
2025-05-27 22:07:10 +0530

    calendar feature

---
+++ src/screens/habits/CalendarScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { CalendarView } from '../../components/calendar/CalendarView';
+
+const CalendarScreen: React.FC = () => {
+  return <CalendarView />;
+};
+
+export default CalendarScreen;
